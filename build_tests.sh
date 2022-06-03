2d6a2a02c84b35e9c5487619cfd954352fcca91f
Richard McCrae <richard.mccrae@nordicsemi.no>
2022-06-03 08:47:47 +0200

    get unity fixtures

--- build_tests.sh
+++ build_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 
-gcc -Iinclude src/getnumber.c -Iunity/src test/Testgetnumber.c unity/src/unity.c -o testgetnumber
+gcc -Iinclude src/getnumber.c \
+    -Iunity/include test/TestRunner.c test/Testgetnumber.c \
+    unity/src/unity.c unity/src/unity_fixture.c \
+    -o testgetnumber
---
+++ test/TestRunner.c
@@ -0,0 +1,12 @@
+#include "unity_fixture.h"
+
+
+static void getnumberTests(void)
+{
+
+}
+
+int main(int argc, const char * argv[])
+{
+    return UnityMain(argc, argv, getnumberTests);
+}
